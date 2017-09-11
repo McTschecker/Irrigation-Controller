c5e5ef2a30b177f0d881ea2d99eac8e756054d73
McTschecker <10790084+McTschecker@users.noreply.github.com>
2017-09-11 18:40:32 -0400

    sensorread

---
+++ sensorread.py
@@ -0,0 +1,20 @@
+
+
+import sys
+import Adafruit_DHT
+import Numpy as np
+
+class temperatur:
+	pin = 0
+	def defpin(pini):
+		pin = pini
+
+	def getTemperature():
+		temperatur = []
+		
+		return temperatur
+	def getHumidity():
+		#
+	def getAll():
+
+
