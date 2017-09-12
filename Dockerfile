a2678ddccc84a07556c89a09cfa57c6d5f18ec68
McTschecker <10790084+McTschecker@users.noreply.github.com>
2017-09-11 20:09:55 -0400

    test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
 FROM resin/rpi-raspbian
 RUN apt -y update && apt -y upgrade && apt -y install python && apt-get install build-essential python-dev git
-RUN pip install GPIO
-RUN git pull https://github.com/adafruit/Adafruit_Python_DHT.git
-RUN cd Adafruit_Python_DHT
-RUN sudo python setup.py install
+RUN pip install GPIO Adafruit_DHT
--- sensorread.py
+++ sensorread.py
@@ -4,17 +4,29 @@ import sys
 import Adafruit_DHT
 import Numpy as np
 
-class temperatur:
-	pin = 0
-	def defpin(pini):
-		pin = pini
+# class temperatur:
+# 	pin = 0
+# 	def defpin(pini):
+# 		pin = pini
 
-	def getTemperature():
-		temperatur = []
+# 	def getTemperature():
+# 		temperatur = []
 		
-		return temperatur
-	def getHumidity():
-		#
-	def getAll():
+# 		return temperatur
+# 	def getHumidity():
+# 		#
+# 	def getAll():
 
+def readDHT(PinDHT, DHT_TYPE=Adafruit_DHT.DHT.22): #sensorargs = DHT TYPE
+	sensor = DHT_TYPE
 
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	humidA = np.Array(humidity)
+	tempA = np.Array(temperature)
+	for i in range(10):
+		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+		humidA = np.append(humidA, humidity)
+		tempA = np.append(tempA, temperatur)
+	humidity = np.mean(humidA)
+	temperature = np.mean(tempA)
+	return humidity, temperature
