4d9306af55c3aaa443625e6f2f3b9c876a7d89c5
McTschecker <10790084+McTschecker@users.noreply.github.com>
2017-09-25 13:45:21 -0400

    update count of reading

--- sensorread.py
+++ sensorread.py
@@ -4,18 +4,9 @@ import sys
 import Adafruit_DHT
 import Numpy as np
 
-# class temperatur:
-# 	pin = 0
-# 	def defpin(pini):
-# 		pin = pini
-
-# 	def getTemperature():
-# 		temperatur = []
-		
-# 		return temperatur
-# 	def getHumidity():
-# 		#
-# 	def getAll():
+numOfReads = 10 # Numbers of sensor readings
+#increase nom of reads by one to get it running the specific times
+numOfReads = numOfReads +1
 
 def readDHT(PinDHT, DHT_TYPE=Adafruit_DHT.DHT.22): #sensorargs = DHT TYPE
 	sensor = DHT_TYPE
@@ -23,7 +14,7 @@ def readDHT(PinDHT, DHT_TYPE=Adafruit_DHT.DHT.22): #sensorargs = DHT TYPE
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 	humidA = np.Array(humidity)
 	tempA = np.Array(temperature)
-	for i in range(10):
+	for i in range(numOfReads):
 		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 		humidA = np.append(humidA, humidity)
 		tempA = np.append(tempA, temperatur)
