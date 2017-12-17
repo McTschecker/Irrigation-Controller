022bf6713dd40ce72d08d6a26b519412d3bfb36c
McTschecker <10790084+McTschecker@users.noreply.github.com>
2017-12-16 23:48:16 -0500

    as a string

--- testSubFunctions.py
+++ testSubFunctions.py
@@ -3,7 +3,7 @@ def testUpdateValues():
     temperature = 1000
     humidity = -10
     moisture = 100000
-    write_key = ENZIKF2E2KS08KD4
+    write_key = "ENZIKF2E2KS08KD4"
     channel_id = 386549
     update(temperature, humidity, moisture, channel_id, write_key)
 
