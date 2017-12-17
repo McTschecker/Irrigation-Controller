4d8f02fbd8b3d4d976f6fcf7961dbea5a6cb3aed
McTschecker <10790084+McTschecker@users.noreply.github.com>
2017-12-17 00:09:50 -0500

    add confirmation message

--- testSubFunctions.py
+++ testSubFunctions.py
@@ -6,5 +6,6 @@ def testUpdateValues():
     write_key = "ENZIKF2E2KS08KD4"
     channel_id = 386549
     update(temperature, humidity, moisture, channel_id, write_key)
+    print("Succesfully put some values to thingspeak")
 
 testUpdateValues()
