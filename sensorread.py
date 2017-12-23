be5ce537faaf5c7d82ac6b781bcdedc670061b4b
Fabian Blank <10790084+McTschecker@users.noreply.github.com>
2017-12-23 17:04:35 -0500

    add online solution

--- sensorread.py
+++ sensorread.py
@@ -12,14 +12,31 @@ numOfReads = 10 # Numbers of sensor readings
 
 def readMoisture():
 # a part of this code is by modmypi https://github.com/modmypi/Moisture-Sensor/blob/master/moisture.py
-	GPIO.setmode(GPIO.BCM)
-	# Define the GPIO pin that we have our digital output from our sensor connected to
-	channel = 3
-	# Set the GPIO pin to an input
 	GPIO.setup(channel, GPIO.IN)
 	SensorValue = GPIO.input(channel)
 	print(SensorValue)
 	return 0
+def initMoisture():
+	# a part of this code is by modmypi https://github.com/modmypi/Moisture-Sensor/blob/master/moisture.py
+	# Set our GPIO numbering to BCM
+	GPIO.setmode(GPIO.BCM)
+
+	# Define the GPIO pin that we have our digital output from our sensor connected to
+	channel = 3
+	# Set the GPIO pin to an input
+	GPIO.setup(channel, GPIO.IN)
+
+	# This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
+	GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+	# This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
+	GPIO.add_event_callback(channel, callback)
+def callback(channel):  
+	if GPIO.input(channel):
+		print "LED off"
+		sendEmail(message_dead)
+	else:
+		print "LED on"
+		sendEmail(message_alive)
 
 
 
