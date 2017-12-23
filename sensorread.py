6533f10e52c461eb4c8657f6910d0ef7a8059cad
Fabian Blank <10790084+McTschecker@users.noreply.github.com>
2017-12-23 17:07:39 -0500

    finish going back

--- sensorread.py
+++ sensorread.py
@@ -10,38 +10,8 @@ import statistics
 
 numOfReads = 10 # Numbers of sensor readings
 
-<<<<<<< HEAD
 def readMoisture():
-# a part of this code is by modmypi https://github.com/modmypi/Moisture-Sensor/blob/master/moisture.py
-	GPIO.setup(channel, GPIO.IN)
-	SensorValue = GPIO.input(channel)
-	print(SensorValue)
-	return 0
-def initMoisture():
-	# a part of this code is by modmypi https://github.com/modmypi/Moisture-Sensor/blob/master/moisture.py
-	# Set our GPIO numbering to BCM
-	GPIO.setmode(GPIO.BCM)
-
-	# Define the GPIO pin that we have our digital output from our sensor connected to
-	channel = 3
-	# Set the GPIO pin to an input
-	GPIO.setup(channel, GPIO.IN)
-
-	# This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
-	GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
-	# This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
-	GPIO.add_event_callback(channel, callback)
-def callback(channel):  
-	if GPIO.input(channel):
-		print "LED off"
-		sendEmail(message_dead)
-	else:
-		print "LED on"
-		sendEmail(message_alive)
-
-=======
-def readmoisture():
-	pot = MCP3008(0)
+	pot = MCP3008(3)
 	print(pot.value)
 	moistureA = [pot.value]
 	for h in range(numOfReads):
@@ -50,7 +20,6 @@ def readmoisture():
 	moisture = s.mean(moistureA)
 	print("average moisture", moisture)
 	return moisture
->>>>>>> parent of d577a5c... add support for temperature sensor
 
 
 def readDHT (pin): 
