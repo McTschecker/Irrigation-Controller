bc4e06af56f52ef3aa173215dd7cbd9e133679d4
Fabian Blank <Fabian@wirsindblank.de>
2017-12-26 14:33:19 -0500

    switch values

--- testSubFunctions.py
+++ testSubFunctions.py
@@ -11,14 +11,14 @@ def testUpdateValues():
     print("Succesfully put some values to thingspeak")
 
 def testWatering():
-    #Test1 mode 1; value 0.49  ====> True
-    test1 = shouldWater(0.49) 
+    #Test1 mode 1; value 0.6  ====> True
+    test1 = shouldWater(0.6) 
     if test1:
         print("Test 1 passed for watering")
     else:
         raise Exception ("Test 1 failed for vlue of 0.49, should be true in mode 1", test1)
-    #Test2 mode 1; value 0.6  ====> False
-    test2 = shouldWater(0.6)
+    #Test2 mode 1; value 0.49  ====> False
+    test2 = shouldWater(0.49)
     if test2:
          raise Exception ("Test 1 failed for vlue of 0.6, should be false in mode 1", test2)
     else: 
@@ -26,14 +26,14 @@ def testWatering():
     ###############################################################
     ###mode 2
     print("Now testing mode 2")
-    #Test3 mode 2; value 0.4  ====> True
-    test1 = shouldWater(0.4, mode=2) 
+    #Test3 mode 2; value 0.9  ====> True
+    test1 = shouldWater(0.9, mode=2) 
     if test1:
         print("Test 1 passed for watering")
     else:
         raise Exception ("Test 3 failed for vlue of 0.4, should be true in mode 2", test1)
-    #Test2 mode 2; value 0.9  ====> False
-    test4 = shouldWater(0.9,mode=2)
+    #Test2 mode 2; value 0.4  ====> False
+    test4 = shouldWater(0.4,mode=2)
     if test2:
          raise Exception ("Test 1 failed for vlue of 0.0, should be false in mode 2", test2)
     else: 
