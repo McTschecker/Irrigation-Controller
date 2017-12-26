a3ee47bb3acf29a9e489e0a05ba0c713ad5b4c3b
Fabian Blank <Fabian@wirsindblank.de>
2017-12-26 13:27:46 -0500

    watering
    auto run the check if watering is neccesary
    add messages, specifying if watering is neccesary

--- watering.py
+++ watering.py
@@ -1,18 +1,22 @@
-moisMode1 = 0.75 ###example Values
-moisMode2 = 0.8  ####example Values
-waterTime = 10
+global moisMode1 = 0.75 ###example Values
+global moisMode2 = 0.8  ####example Values
+global waterTime = 10
 def shouldWater(mode=1,moisture):
     if mode==1:
         if moisture>= moisMode1:
+            print("Watering neccesary, Watering now")
             water(waterTime)
             return true
         else:
+            print("Watering not neccesary")
             return false
     elif mode== 2:
         if moisture>= moisMode2:
+            print("Watering neccesary, Watering now")
             water(waterTime)
             return true
         else:
+            print("Watering not neccesary")
             return false
     else:
         raise exeption("Unknown Mode, please select an configured mode!")
