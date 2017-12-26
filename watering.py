5ac772a37689c5ab221c41daf9b6a9f4ce21c014
Fabian Blank <Fabian@wirsindblank.de>
2017-12-26 13:23:33 -0500

    watering basics

---
+++ watering.py
@@ -0,0 +1,23 @@
+moisMode1 = 0.75 ###example Values
+moisMode2 = 0.8  ####example Values
+waterTime = 10
+def shouldWater(mode=1,moisture):
+    if mode==1:
+        if moisture>= moisMode1:
+            water(waterTime)
+            return true
+        else:
+            return false
+    elif mode== 2:
+        if moisture>= moisMode2:
+            water(waterTime)
+            return true
+        else:
+            return false
+    else:
+        raise exeption("Unknown Mode, please select an configured mode!")
+
+
+
+def water(s):
+    ##code for watering here
