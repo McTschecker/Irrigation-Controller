d51971c155ebed22230f9dbc394b083143d397c6
Fabian Blank <Fabian@wirsindblank.de>
2017-12-27 17:23:14 -0500

    comment

--- water.py
+++ water.py
@@ -2,7 +2,7 @@ def water(s):
     from gpiozero import LED
     ##code for watering here
     print("Watering neccesary, Watering now")
-    led on GPIO pin 25
+    #led on GPIO pin 25
     led = LED(25)
     led.on()
     time.sleep(s, pin_factory=factory)
