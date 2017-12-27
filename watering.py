c77d644b07050f442e77cde0f521b5f93ad57756
Fabian Blank <Fabian@wirsindblank.de>
2017-12-27 17:13:34 -0500

    comment out for testing purposees

--- watering.py
+++ watering.py
@@ -45,7 +45,7 @@ def water(s):
     ##code for watering here
     print("Watering neccesary, Watering now")
     #led on GPIO pin 25
-    led = LED(25)
-    led.on()
-    time.sleep(waterTime, pin_factory=factory)
-    led.off()
+    # led = LED(25)
+    # led.on()
+    # time.sleep(waterTime, pin_factory=factory)
+    # led.off()
