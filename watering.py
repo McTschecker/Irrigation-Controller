f266c5556d9d215a1266cc3f4b7ae4c551019ff2
Fabian Blank <Fabian@wirsindblank.de>
2017-12-27 17:04:11 -0500

    add with test

--- watering.py
+++ watering.py
@@ -5,7 +5,22 @@ moisMode2 = 0.75  ####example Values
 waterTime = 10
 def shouldWater(moisture, mode=1, test=False):
     if test:
-        an
+        if mode==1:
+             if moisture>= moisMode1:
+                print("Watering neccesary, Watering now")
+                return True
+            else:
+                print("Watering not neccesary") 
+                return False
+        elif mode== 2:
+            if moisture>= moisMode2:
+                print("Watering neccesary, Watering now")
+                return True
+            else:
+                print("Watering not neccesary")
+                return False
+        else:
+            raise exeption("Unknown Mode, please select an configured mode!")
     else:
         if mode==1:
              if moisture>= moisMode1:
