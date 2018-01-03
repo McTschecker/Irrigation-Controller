dfdc0d7cea8fea87e1b187670301fc2609a7bfaf
Fabian Blank <Fabian@wirsindblank.de>
2018-01-03 15:41:05 -0500

    comments for  update values

--- updatevalues.py
+++ updatevalues.py
@@ -1,5 +1,4 @@
 def update(temperature, humidity, moisture,channe_id, write_key):
-    import requests
-    url = "https://api.thingspeak.com/update?api_key="+ write_key + "&field1=" + str(temperature) + "&field2=" + str(humidity) + "&field3=" + str(moisture)
-    r = requests.get(url)
-
+    import requests #import the lib for requests
+    url = "https://api.thingspeak.com/update?api_key="+ write_key + "&field1=" + str(temperature) + "&field2=" + str(humidity) + "&field3=" + str(moisture) #build the URL
+    r = requests.get(url) #open the url
