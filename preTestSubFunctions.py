71cf6182e862af24b9a0c8dc88956196de8195e1
Fabian Blank <Fabian@wirsindblank.de>
2018-01-03 15:29:00 -0500

    comments

--- preTestSubFunctions.py
+++ preTestSubFunctions.py
@@ -2,11 +2,14 @@ from updatevalues import update
 import random
 from watering import shouldWater
 def testUpdateValues():
+    #create random values
     temperature = random.randint(0,500)
     humidity = random.randint(0,100)
     moisture = random.randint(0,10000)
+    #needed keys for thingspeak
     write_key = "ENZIKF2E2KS08KD4"
     channel_id = 386549
+    #publish the random values
     update(temperature, humidity, moisture, channel_id, write_key)
     print("Succesfully put some values to thingspeak")
 
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#install DHT lib
 pip install Adafruit_Python_DHT
+#run tests for watering
 python part2SubFunctions.py
+#execute script
 python main.py
